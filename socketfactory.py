c4fea3a5058277bb50998b6d035b07d236d17b1f
spuriousdata <spuriousdata@4c7ce805-4811-4d71-8950-e1dbdd91b317>
2009-06-21 20:10:10 +0000

    wrote basic working of socketfactory

--- socketfactory.py
+++ socketfactory.py
@@ -3,6 +3,9 @@ import socks
 from main import config
 
 class socketfactory:
-    def __init__(self, family=2, type=1, proto=0, _sock=None):
-        if config.get('socks', 'use_socks'
+    @classmethod
+    def get_socket(family=2, type=1, proto=0, _sock=None):
+        if config.has_section('socks'):
+            return socks.socks(family, type, proto, _sock)
+        else: return socket.socket(family, type, proto, _sock)
 

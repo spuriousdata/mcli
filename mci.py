f1dcff7b2c4266b70784768367e351cf5c2ba2a6
spuriousdata <spuriousdata@4c7ce805-4811-4d71-8950-e1dbdd91b317>
2009-06-21 14:43:43 +0000

    fixed old-style signal/slot connections

--- mci.py
+++ mci.py
@@ -1,56 +1,44 @@
 from PyQt4 import QtGui as qt, QtCore as core
-from ui_mci import Ui_MciClass
-from connect_dialog import connect_dialog
+from ui_mci import Ui_McIClass
+from connect_dialog import ConnectDialog
 
 class Mci(qt.QMainWindow):
     def __init__(self, parent):
         qt.QMainWindow.__init__(self)
-        self.ui = Ui_MciClass()
+        self.ui = Ui_McIClass()
         self.ui.setupUi(self)
 
-        self.connect(self.ui.action_quick_connect, core.SIGNAL(triggered()), self, core.SLOT(self.openConnectDialog()))
-        self.connect(self.ui.add_button, core.SIGNAL(clicked()), self, core.SLOT(self.addClicked()))
-        self.connect(self.ui.delete_button, core.SIGNAL(clicked()), self, core.SLOT(self.deleteClicked()))
-        self.connect(self.ui.get_button, core.SIGNAL(clicked()), self, core.SLOT(self.getClicked()))
-        self.connect(self.ui.flushall_button, core.SIGNAL(clicked()), self, core.SLOT(self.flushallClicked()))
+        self.connect(self.ui.action_quick_connect, core.SIGNAL("triggered()"), self, core.SLOT("openConnectDialog()"))
+        self.connect(self.ui.add_button, core.SIGNAL("clicked()"), self, core.SLOT("addClicked()"))
+        self.connect(self.ui.delete_button, core.SIGNAL("clicked()"), self, core.SLOT("deleteClicked()"))
+        self.connect(self.ui.get_button, core.SIGNAL("clicked()"), self, core.SLOT("getClicked()"))
+        self.connect(self.ui.flushall_button, core.SIGNAL("clicked()"), self, core.SLOT("flushallClicked()"))
 
-    def openConnectDialog()
+    def openConnectDialog(self):
         if self.connect_dialog == None:
             self.connect_dialog = ConnectDialog(self)
-    
-        connect_dialog->show()
-        connect_dialog->raise()
-        connect_dialog->activateWindow()
 
-        self.connect(self.connect_dialog.mc, core.SIGNAL(hasNewStats()), self, core.SLOT(displayStats()))
-}
+        self.connect_dialog.show()
+        self.connect_dialog.raise_()
+        self.connect_dialog.activateWindow()
 
-void McI::displayStats()
-{
+        self.connect(self.connect_dialog.mc, core.SIGNAL("hasNewStats()"), self, core.SLOT("displayStats()"))
 
-}
+    def displayStats(self):
+        pass
 
-void McI::addClicked()
-{
-    qDebug("addClicked() called")
-}
+    def addClicked(self):
+        print "addClicked() called"
 
-void McI::getClicked()
-{
-    qDebug("getClicked() called")
-}
+    def getClicked(self):
+        print "getClicked() called"
 
-void McI::deleteClicked()
-{
-    qDebug("deleteClicked() called")
-}
-void McI::flushallClicked()
-{
-    qDebug("flushallClicked() called")
-}
+    def deleteClicked(self):
+        print "deleteClicked() called"
 
-McI::~McI()
-{
-    delete ui
-    delete connect_dialog
-}
+    def flushallClicked(self):
+        print "flushallClicked() called"
+
+    def __del__(self):
+        del self.ui
+        del self.connect_dialog

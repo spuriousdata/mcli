205bd25d6dae5fbb711db33b42bb0a5b731d2006
spuriousdata <spuriousdata@4c7ce805-4811-4d71-8950-e1dbdd91b317>
2009-06-21 20:11:30 +0000

    fleshed out memcache class

--- memcache.py
+++ memcache.py
@@ -1,13 +1,24 @@
-import sys
-import socket
+from socketfactory import socketfactory
+from main import config
 
 class Memcache:
-    def __init__(self, servers = None):
-        self.sockets = {}
-        self.verbose = False
-        if servers is None:
-            servers = {}
-        for server in servers:
-            socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            socket.connect
+    def __init__(self, servers = []):
+        self.sockets = []
+        self.servers = servers
 
+    def connect(self):
+        for server in self.servers:
+            socket = socketfactory.get_socket(socket.AF_INET, socket.SOCK_STREAM)
+            socket.connect(server.host, server.port)
+            self.sockets.append(socket)
+
+    def disconnect(self):
+        for socket in self.sockets:
+            socket.close()
+            del socket
+
+    def __del__(self):
+        for s in self.sockets:
+            del s
+        for s in self.servers:
+            del s

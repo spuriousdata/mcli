4683dc094e68f2a86ca2d1bf98a344d0285ce3e2
spuriousdata <spuriousdata@4c7ce805-4811-4d71-8950-e1dbdd91b317>
2009-06-22 04:05:44 +0000

    moved config into it's own module

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 from PyQt4 import QtGui as qt
 from mci import Mci
-from Config import Configuration
-from getopt import getopt, GetoptError
 import sys
 
-config = None
 
 def main():
-    global config
-    config = Configuration()
     a = qt.QApplication(sys.argv)
     w = Mci(a)
     w.show()

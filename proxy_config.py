fef77621dd5e8da99771d3f65eeb2404bfe1370b
spuriousdata <spuriousdata@4c7ce805-4811-4d71-8950-e1dbdd91b317>
2009-06-22 04:00:15 +0000

    Adding proxy config class

---
+++ proxy_config.py
@@ -0,0 +1,26 @@
+from ui_proxy_config import Ui_ProxyConfig
+from PyQt4 import QtGui as qt, QtCore as core
+from Config import config
+
+class ProxyConfig(qt.QDialog):
+    def __init__(self, parent):
+        qt.QDialog.__init__(self, parent)
+        self.ui = Ui_ProxyConfig()
+        self.ui.setupUi(self)
+
+        self.ui.socks_proto.valueChanged[int].connect(self.toggle_unpw)
+        self.ui.socks_port.setValidator(
+            qt.QRegExpValidator(core.QRegExp("[1-9][0-9]{0,4}"), self)
+        )
+
+    def toggle_unpw(self, val):
+        if config.get('general', 'verbose'):
+            print "socks protocol version is %d" % val
+        enable = False
+        if val == 5:
+            enable = True
+        self.ui.socks_username_label.setEnabled(enable)
+        self.ui.socks_username.setEnabled(enable)
+        self.ui.socks_password_label.setEnabled(enable)
+        self.ui.socks_password.setEnabled(enable)
+

ad40e9c495c83d46c8e5c8ee8e532a4ab667d7d8
Mike O'Malley <spuriousdata@gmail.com>
2012-11-26 17:48:09 -0500

    moved to rewrite branch

--- cmd/src/util.h
+++
@@ -1,8 +0,0 @@
-#ifndef __UTIL_H__
-#define __UTIL_H__
-
-std::string rtrim(std::string s);
-std::string ltrim(std::string s);
-std::string trim(std::string s);
-
-#endif
--- qt/HostEntry.cc
+++
@@ -1,9 +0,0 @@
-#include "HostEntry.h"
-
-QRegExp HostEntry::port_regex = QRegExp("[1-9][0-9]{0,4}");
-
-HostEntry::~HostEntry()
-{
-    delete host;
-    delete port;
-}
